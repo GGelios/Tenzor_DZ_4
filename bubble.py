50a900875248863f30f8b6ddb75e644cc8fe3f84
GGelios <119353367+GGelios@users.noreply.github.com>
2022-12-12 20:58:25 +0300

    Add files via upload

---
+++ bubble.py
@@ -0,0 +1,10 @@
+import random
+a=[]
+for i in range(0,10):
+    a.append(random.randint(-10,10))
+print('Unsorted list:', a)
+for i in range((len(a)-1)):
+    for j in range((len(a)-1)):
+        if a[j]>a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+print('Sorted list:', a)
---
+++ inventory.py
@@ -0,0 +1,28 @@
+weight=0
+inventory={'DragonLore':(6), 'Bread':(0.5), 'Dorogoi Dnevnik...':(0.3), 'Koshka':(1), 'Butterfly':(0.0)} 
+while True:
+    choose=input('''What you want to do?
+    1. Add item
+    2. Delete item
+    3. View inventory
+    4. Exit
+    ''')
+    weight=sum(inventory.values())
+    print(weight,'/100', sep='')
+    if weight>100:
+        print('You are overloaded')
+    elif choose=='1' and weight<100:
+        dweight=int(input('Input weight: '))
+        weight=weight+dweight
+        if weight<100:
+            inventory[input('Enter a name ')]=dweight
+        else:
+            print('Error')
+    elif choose=='2':
+        inventory.pop(input('What do you want to delete '))
+    elif choose=='3':
+        print(inventory)
+    elif choose=='4':
+        break
+    else:
+        print("Error")
