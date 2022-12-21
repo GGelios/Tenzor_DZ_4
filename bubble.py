1cc1a5caf7ff9d78ab3c222aa3a21a7ca5c4cf2e
GGelios <119353367+GGelios@users.noreply.github.com>
2022-12-21 19:42:06 +0300

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,12 +1,14 @@
 """Реализовать сортировку списка методом пузырька."""
 
 import random
-a=[]
-for i in range(0,10):
-    a.append(random.randint(-10,10))
+
+a = []
+for i in range(0, 10):
+    a.append(random.randint(-10, 10))
 print('Unsorted list:', a)
-for i in range((len(a)-1)):
-    for j in range((len(a)-1)):
-        if a[j]>a[j+1]:
+for i in range((len(a) - 1)):
+    for j in range((len(a) - 1)):
+        if a[j] > a[j+1]:
             a[j], a[j+1] = a[j+1], a[j]
+            
 print('Sorted list:', a)
