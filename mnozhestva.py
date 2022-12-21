c81179e1773edeac6d45a8fb4e5b788e3633d310
GGelios <119353367+GGelios@users.noreply.github.com>
2022-12-21 19:51:36 +0300

    Update mnozhestva.py

--- mnozhestva.py
+++ mnozhestva.py
@@ -5,15 +5,19 @@
        4. входят в первое или во второе, но не в оба из них одновременно."""
 
 import random
-a=[]
-b=[]
-for i in range(0,10):
-    a.append(random.randint(-10,10))
-    b.append(random.randint(-10,10))
-a=set(a)
-b=set(b)
+
+a = []
+b = []
+
+for i in range(0, 10):
+    a.append(random.randint(-10, 10))
+    b.append(random.randint(-10, 10))
+
+a = set(a)
+b = set(b)
+
 print('First set', a, 'Second set', b)
-print('Include a and b', a&b)
-print('Include in a, not include in b', a-b)
-print('Include in b, not include in a', b-a)
-print('Include in a or b, but not in both', a^b)
+print('Include a and b', a & b)
+print('Include in a, not include in b', a - b)
+print('Include in b, not include in a', b - a)
+print('Include in a or b, but not in both', a ^ b)
