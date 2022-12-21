a4df060d50c7d8bb5cc08a638e2be236b1666475
GGelios <119353367+GGelios@users.noreply.github.com>
2022-12-21 19:50:27 +0300

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,30 +1,33 @@
 """Создать игровой инвентарь. Должна быть возможность добавлять в него предметы и удалять предметы из него. Инвентарь должен быть ограничен по весу, каждый предмет имеет свой вес. Вывод предметов должен быть с названием предмета и его весом."""
 
-weight=0
-inventory={'DragonLore':(6), 'Bread':(0.5), 'Dorogoi Dnevnik...':(0.3), 'Koshka':(1), 'Butterfly':(0.0)} 
+weight = 0
+inventory = {'DragonLore':(6), 'Bread':(0.5), 'Dorogoi Dnevnik...':(0.3),
+             'Koshka':(1), 'Butterfly':(0.0)} 
+
 while True:
-    choose=input('''What you want to do?
+    choose = input('''What you want to do?
     1. Add item
     2. Delete item
     3. View inventory
     4. Exit
     ''')
-    weight=sum(inventory.values())
-    print(weight,'/100', sep='')
-    if weight>100:
+    weight = sum(inventory.values())
+    print(weight, '/100', sep='')
+    
+    if weight > 100:
         print('You are overloaded')
-    elif choose=='1' and weight<100:
-        dweight=int(input('Input weight: '))
-        weight=weight+dweight
-        if weight<100:
-            inventory[input('Enter a name ')]=dweight
+    elif choose == '1' and weight < 100:
+        dweight = int(input('Input weight: '))
+        weight = weight + dweight
+        if weight < 100:
+            inventory[input('Enter a name ')] = dweight
         else:
             print('Error')
-    elif choose=='2':
+    elif choose == '2':
         inventory.pop(input('What do you want to delete '))
-    elif choose=='3':
+    elif choose == '3':
         print(inventory)
-    elif choose=='4':
+    elif choose == '4':
         break
     else:
         print("Error")
